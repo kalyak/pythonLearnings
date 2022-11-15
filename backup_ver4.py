400fbc8f073300fe80ad0e1ffe20cba07b7817b7
kalyakusumadi <kalyakusumadi@gmail.com>
2022-11-15 23:12:21 +0800

    Edit zip filename to accept comment from input.

--- backup_ver4.py
+++ backup_ver4.py
@@ -1,7 +1,21 @@
 import pathlib
 import zipfile
+import time
+import os
 
 directory = pathlib.Path(r'/Users/kalyakusumadi/Desktop')
+
+target_dir = '/Users/kalyakusumadi/backup'
+
+# Create target directory if it is not present
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)  # make directory
+
+# The files are backed up into a zip file.
+# The current day is the name of the subdirectory
+# in the main directory.
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+now = time.strftime('%H%M%S')
 
 # Take a comment from the user to
 # create the name of the zip file
@@ -13,7 +27,12 @@ else:
     target = today + os.sep + now + '_' +\
              comment.replace(' ','_') + '.zip'
 
-with zipfile.ZipFile(r"/Users/kalyakusumadi/backup/pythonzipfile.zip", mode="w") as archive:
+# Create the subdirectory if it isn't already there
+if not os.path.exists(today):
+    os.mkdir(today)  # make subdirectory
+    print('Successfully created directory', today)
+
+with zipfile.ZipFile(target, mode="w") as archive:
     errorfiles = []
     for file_path in directory.rglob("*"):
         try:
